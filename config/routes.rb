b9706cc9b48baf071dd4e5223b98ee833bacfdd8
dixpac <dino.onex@gmail.com>
2018-04-17 10:39:33 +0200

    Fix bug in loading avatar in answers

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
-require 'sidekiq/web'
-
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-
-  mount Sidekiq::Web => '/sidekiq'
 
   root "questions#index"
 
   resources :questions do
-    resources :answers
+    resources :answers, except: [:index, :show]
   end
 
   post   'likes/:sgid', to: 'likes#create', as: :likes
